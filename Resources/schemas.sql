82dd4371bfbc81eb2158d154f5567b0555564326
Jinbaek0823 <jinbaek0823@gmail.com>
2024-08-23 14:13:48 -0400

    ERD_SCHEMAS updated

--- Resources/schemas.sql
+++ Resources/schemas.sql
@@ -1,25 +1,25 @@
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-DROP table if exists attackers_data;
-DROP table if exists physical_data;
-DROP table if exists goalkeeper_data;
-DROP table if exists defensive_data;
-DROP table if exists player_info;
+DROP TABLE IF EXISTS attackers_data;
+DROP TABLE IF EXISTS physical_data;
+DROP TABLE IF EXISTS goalkeeper_data;	
+DROP TABLE IF EXISTS defensive_data;
+DROP TABLE IF EXISTS player_info;
 
 CREATE TABLE "player_info" (
     "id" INT   NOT NULL,
-    "player_name" VARCHAR(255)   NOT NULL,
-    "birthday" DATE   NOT NULL,
-    "age" INT   NOT NULL,
-    "height" FLOAT   NOT NULL,
+    "name" VARCHAR(255)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "height_cm" FLOAT   NOT NULL,
     "weight_kgs" FLOAT   NOT NULL,
-    "overall_rating" FLOAT   NOT NULL,
-    "potential" FLOAT   NOT NULL,
-    "positions" VARCHAR(255)   NOT NULL,
+    "overall_rating" INT   NOT NULL,
+    "potential" INT   NOT NULL,
     "nationality" VARCHAR(255)   NOT NULL,
     "value_euro" FLOAT   NOT NULL,
     "wage_euro" FLOAT   NOT NULL,
+    "age" INT   NOT NULL,
+    "position" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_player_info" PRIMARY KEY (
         "id"
      )
@@ -27,52 +27,52 @@ CREATE TABLE "player_info" (
 
 CREATE TABLE "attackers_data" (
     "id" INT   NOT NULL,
-    "crossing" FLOAT   NOT NULL,
-    "finishing" FLOAT   NOT NULL,
-    "heading_accuracy" FLOAT   NOT NULL,
-    "short_passing" FLOAT   NOT NULL,
-    "volleys" FLOAT   NOT NULL,
-    "dribbling" FLOAT   NOT NULL,
-    "curve" FLOAT   NOT NULL,
-    "free_kick_accuracy" FLOAT   NOT NULL,
-    "long_passing" FLOAT   NOT NULL,
-    "ball_control" FLOAT   NOT NULL,
-    "shot_power" FLOAT   NOT NULL,
-    "long_shots" FLOAT   NOT NULL
+    "crossing" INT   NOT NULL,
+    "finishing" INT   NOT NULL,
+    "heading_accuracy" INT   NOT NULL,
+    "short_passing" INT   NOT NULL,
+    "volleys" INT   NOT NULL,
+    "dribbling" INT   NOT NULL,
+    "curve" INT   NOT NULL,
+    "freekick_accuracy" INT   NOT NULL,
+    "long_passing" INT   NOT NULL,
+    "ball_control" INT   NOT NULL,
+    "shot_power" INT   NOT NULL,
+    "long_shots" INT   NOT NULL
 );
 
 CREATE TABLE "physical_data" (
     "id" INT   NOT NULL,
-    "reactions" FLOAT   NOT NULL,
-    "balance" FLOAT   NOT NULL,
-    "jumping" FLOAT   NOT NULL,
-    "stamina" FLOAT   NOT NULL,
-    "strength" FLOAT   NOT NULL,
-    "aggression" FLOAT   NOT NULL
+    "reactions" INT   NOT NULL,
+    "balance" INT   NOT NULL,
+    "jumping" INT   NOT NULL,
+    "stamina" INT   NOT NULL,
+    "strength" INT   NOT NULL,
+    "aggression" INT   NOT NULL
 );
 
 CREATE TABLE "goalkeeper_data" (
     "id" INT   NOT NULL,
-    "agility" FLOAT   NOT NULL,
-    "positioning" FLOAT   NOT NULL,
-    "reactions" FLOAT   NOT NULL,
-    "jumping" FLOAT   NOT NULL,
-    "strength" FLOAT   NOT NULL,
-    "balance" FLOAT   NOT NULL,
-    "stamina" FLOAT   NOT NULL
+    "agility" INT   NOT NULL,
+    "positioning" INT   NOT NULL,
+    "reactions" INT   NOT NULL,
+    "jumping" INT   NOT NULL,
+    "strength" INT   NOT NULL,
+    "balance" INT   NOT NULL,
+    "stamina" INT   NOT NULL
 );
 
 CREATE TABLE "defensive_data" (
     "id" INT   NOT NULL,
-    "aggression" FLOAT   NOT NULL,
-    "interceptions" FLOAT   NOT NULL,
-    "positioning" FLOAT   NOT NULL,
-    "marking" FLOAT   NOT NULL,
-    "long_shots" FLOAT   NOT NULL,
-    "standing_tackle" FLOAT   NOT NULL,
-    "short_passing" FLOAT   NOT NULL,
-    "long_passing" FLOAT   NOT NULL,
-    "sliding_tackle" FLOAT   NOT NULL
+    "aggression" INT   NOT NULL,
+    "interceptions" INT   NOT NULL,
+    "positioning" INT   NOT NULL,
+    "marking" INT   NOT NULL,
+    "long_shots" INT   NOT NULL,
+    "standing_tackle" INT   NOT NULL,
+    "short_passing" INT   NOT NULL,
+    "long_passing" INT   NOT NULL,
+    "sliding_tackle" INT   NOT NULL
 );
 
 ALTER TABLE "attackers_data" ADD CONSTRAINT "fk_attackers_data_id" FOREIGN KEY("id")
@@ -86,3 +86,4 @@ REFERENCES "player_info" ("id");
 
 ALTER TABLE "defensive_data" ADD CONSTRAINT "fk_defensive_data_id" FOREIGN KEY("id")
 REFERENCES "player_info" ("id");
+
