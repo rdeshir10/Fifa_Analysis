a55a60528b147584f794ee0e34be51c77040373b
Dhruv Saini <dhruvsaini37@hotmail.com>
2024-08-27 18:14:51 -0400

    FinalPush

--- Resources/Queries by dhruv.sql
+++ Resources/Queries by dhruv.sql
@@ -1,154 +1,164 @@
--- 1. AVG rating by position
-
-SELECT position, AVG(overall_rating) AS avg_position_rating
-FROM player_info
-GROUP BY position
-ORDER BY avg_position_rating DESC;
 
 
--- 2. MAX rating at each position
+--1. AVG 
 
-SELECT pi.position, pi.name, pi.overall_rating AS max_position_rating
-FROM player_info pi
-INNER JOIN (
-    SELECT position, MAX(overall_rating) AS max_rating
-    FROM player_info
-    GROUP BY position
-) sub
-ON pi.position = sub.position
-AND pi.overall_rating = sub.max_rating
-ORDER BY max_position_rating DESC;
-
-
---3.Avg players rating by age
-
-SELECT 
-    CASE 
-        WHEN age < 20 THEN 'Under 20'
-        WHEN age BETWEEN 20 AND 29 THEN '20-29'
-        WHEN age BETWEEN 30 AND 39 THEN '30-39'
-        ELSE '40 and above'
-    END AS age_group,
-    AVG(overall_rating) AS avg_rating
-FROM player_info
-GROUP BY age_group
-ORDER BY age_group;
-
-
--- 4. Top 10 performers by skill(Dribbling) 
-SELECT 
-    pi.id,
-    pi.name,
-    pi.position,
-    ad.dribbling
-FROM 
-    player_info pi
-INNER JOIN 
-    attackers_data ad
-ON 
-    pi.id = ad.id
-ORDER BY 
-    ad.dribbling DESC
-LIMIT 10;
-
-
--- 5. AVG wage by position
-
+-- a. AVG wage by position
 SELECT position, ROUND (AVG(wage_euro)::numeric, 2) AS avg_position_wage
 FROM player_info
 GROUP BY position
 ORDER BY avg_position_wage DESC;
 
-
--- 6. AVG value by position
-
+-- b. AVG value by position
 SELECT position, ROUND (AVG(value_euro)::numeric, 2) AS avg_position_value
 FROM player_info
 GROUP BY position
 ORDER BY avg_position_value DESC;
 
-
--- 7. AVG wage by overall_rating
-
-SELECT overall_rating, ROUND (AVG(wage_euro)::numeric, 2) AS avg_rating_wage
+-- c. AVG rating by position
+SELECT position, ROUND(AVG(overall_rating)::numeric, 2) AS avg_position_rating
 FROM player_info
-GROUP BY overall_rating
-ORDER BY avg_rating_wage DESC;
+GROUP BY position
+ORDER BY avg_position_rating DESC;
 
 
--- 8. AVG value by overall_rating
+--2. Impact of Age on Wage/Value
 
-SELECT overall_rating, ROUND (AVG(value_euro)::numeric, 2) AS avg_rating_value
+--a.Wage
+SELECT age,
+	ROUND (AVG(wage_euro)::numeric, 2) AS avg_wage
 FROM player_info
-GROUP BY overall_rating
-ORDER BY avg_rating_value DESC;
+GROUP BY age
+ORDER BY avg_wage DESC;
 
 
--- 9. MAX wage at each position
+--b.Value
+SELECT age,
+	ROUND (AVG(value_euro)::numeric, 2) AS avg_value
+FROM player_info
+GROUP BY age
+ORDER BY avg_value DESC;
 
-SELECT
-    position,
-    name,
-    wage_euro AS max_position_wage
-FROM
-    player_info
-WHERE
-    (position, wage_euro) IN (
-        SELECT
-            position,
-            MAX(wage_euro)
-        FROM
-            player_info
-        GROUP BY
-            position
-    )
-ORDER BY
-    max_position_wage DESC;
+--3. Top Players by Improvement Potential 
 
+--a.Players with High Improvement Potential
 
--- 10. MAX value at each position
+SELECT name, position, overall_rating, potential,
+    (potential - overall_rating) AS improvement_potential
+FROM player_info
+ORDER BY improvement_potential DESC
+LIMIT 10;
 
-SELECT pi.position, pi.name, pi.value_euro AS max_position_value
-FROM player_info pi
-INNER JOIN (
-    SELECT position, MAX(value_euro) AS max_value
-    FROM player_info
-    GROUP BY position
-) sub
-ON pi.position = sub.position
-AND pi.value_euro = sub.max_value
-ORDER BY max_position_value DESC;
+--b.Young Players with High Potential
 
---11. Underperforming players
-WITH ranked_players 
-AS ( SELECT name, position, overall_rating, wage_euro, value_euro, 
-ROW_NUMBER() OVER (PARTITION BY position ORDER BY overall_rating ASC, wage_euro DESC) 
-AS rank FROM player_info
-WHERE wage_euro IS NOT NULL ) 
-SELECT name, position, overall_rating, wage_euro, value_euro 
-FROM ranked_players 
-WHERE rank <= 10 
-ORDER BY position, rank
-LIMIT 15;
-
---12. Overratted players
-
-SELECT name, overall_rating, wage_euro, value_euro 
-FROM player_info 
-WHERE wage_euro IS NOT NULL 
-AND value_euro IS NOT NULL
-ORDER BY overall_rating ASC, value_euro DESC
+SELECT name, position, overall_rating, potential, age,
+    (potential - overall_rating) AS improvement_potential
+FROM player_info
+WHERE age < 25
+ORDER BY improvement_potential DESC
 LIMIT 10;
 
 
-SELECT name, age, overall_rating, potential, wage_euro, value_euro, (value_euro / wage_euro)/1000 AS value_to_wage_ratio 
-FROM player_info 
-WHERE wage_euro IS NOT NULL 
-AND value_euro IS NOT NULL
-ORDER BY value_to_wage_ratio DESC LIMIT 10;
+-- 4. Most value for money players
+
+-- a. Most value for money attacking players
+WITH AttackingAbility AS (
+    SELECT pi.id,
+           (ad.finishing + ad.volleys +
+            ad.heading_accuracy + ad.dribbling + ad.curve +
+            ad.shot_power + ad.long_shots) / 7.0 AS attacking_ability
+    FROM attackers_data AS ad
+    INNER JOIN player_info AS pi
+    ON ad.id = pi.id
+)
+
+SELECT pi.name, 
+       pi.position, 
+       pi.wage_euro, 
+       pi.value_euro,
+       ROUND(aa.attacking_ability, 0) AS attacking_ability,  
+       ROUND((pi.value_euro / aa.attacking_ability)::numeric, 2) AS value_ratio
+FROM player_info AS pi
+INNER JOIN AttackingAbility AS aa
+ON pi.id = aa.id
+ORDER BY value_ratio ASC
+LIMIT 10;
+
+
+-- b. Most value for money defensive players
+WITH DefensiveAbility AS (
+    SELECT pi.id,
+           (dd.aggression + dd.interceptions + 
+            dd.positioning + dd.marking + dd.standing_tackle +
+            dd.sliding_tackle) / 6.0 AS defensive_ability
+    FROM defensive_data AS dd
+    INNER JOIN player_info AS pi
+    ON dd.id = pi.id
+)
+
+SELECT pi.name, 
+       pi.position, 
+       pi.wage_euro, 
+       pi.value_euro,
+       ROUND(da.defensive_ability, 0) as defensive_ability,
+	ROUND((pi.value_euro / da.defensive_ability)::numeric, 2) AS value_ratio
+FROM player_info AS pi
+INNER JOIN DefensiveAbility AS da
+ON pi.id = da.id
+ORDER BY value_ratio ASC
+LIMIT 10;
 
 
 
 
 
 
+--5.DreamXI 4-3-3
+
+WITH categorized_players AS (
+    SELECT
+        name,
+        position,
+        overall_rating,
+        CASE
+            WHEN position IN ('CB') THEN 'CB'
+			WHEN position IN ('LB', 'LWB') THEN 'LB'
+			WHEN position IN ('RB', 'RWB') THEN 'RB'
+            WHEN position IN ('CM', 'CDM') THEN 'CM'
+			WHEN position IN ('LM') THEN 'LM'
+			WHEN position IN ('RM') THEN 'RM'
+            WHEN position IN ('LW') THEN 'LW'
+			WHEN position IN ('RW') THEN 'RW'
+			WHEN position IN ('ST', 'CF') THEN 'ST'
+			WHEN position IN ('GK') THEN 'GK'
+            ELSE 'Other'
+        END AS player_role
+    FROM player_info
+),
+ranked_players AS (
+    SELECT
+        name,
+        position,
+        overall_rating,
+        player_role,
+        ROW_NUMBER() OVER (PARTITION BY player_role ORDER BY overall_rating DESC) AS rank
+    FROM categorized_players
+    WHERE player_role IN ('GK', 'RB', 'CB', 'LB', 'RM', 'CM', 'LM', 'RW', 'LW', 'ST')
+)
+-- Select the top  Defenders,  Midfielders, and Attackers
+SELECT
+    name,
+    position,
+    overall_rating,
+    player_role
+FROM ranked_players
+WHERE
+    (player_role = 'GK' AND rank <= 1) OR
+    (player_role = 'RB' AND rank <= 1) OR
+	(player_role = 'CB' AND rank <= 2) OR 
+	(player_role = 'LB' AND rank <= 1) OR
+	(player_role = 'RM' AND rank <= 1) OR
+	(player_role = 'CM' AND rank <= 1) OR
+	(player_role = 'LM' AND rank <= 1) OR
+	(player_role = 'RW' AND rank <= 1) OR
+	(player_role = 'ST' AND rank <= 1) OR
+	(player_role = 'LW' AND rank <= 1);
